4d4b6c762648050c246317fe5b40f0b27b78ce4b
kegcheck <65734212+kegcheck@users.noreply.github.com>
2022-10-31 20:26:05 -0500

    fix email again

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -10,11 +10,11 @@ const ContactPage = () => {
                 <Col className='d-flex justify-content-center align-items-center px-4 flex-column'>
                     <h1>Let's talk!</h1>
                     <div className='d-flex flex-row justify-content-center align-items-center px-4'>
-                        <FiMail style={{width: '5em', height: '5em'}} className='mx-3'/>
+                        <FiMail style={{width: '3em', height: '3em'}} className='mx-3'/>
                         <p className='my-0 fs-3 fw-light' style={{letterSpacing: '2px', fontFamily: 'sans-serif'}}>dwyerjakej@gmail.com</p>
                     </div>
                     <Form className='w-100' action="https://formsubmit.co/31b3c32ff56df3ad8800eb7f3d8a14fc" method="POST">
-                        <input type="hidden" name="_next" value="https://www.jakedwyer.com/success"></input>
+                        <input type="hidden" name="_next" value="jakedwyer.com/success"></input>
                         <Form.Group className='mb-3'>
                             <Form.Label>Name</Form.Label>
                             <Form.Control type="text" name="name" required placeholder='Enter your name'></Form.Control>
