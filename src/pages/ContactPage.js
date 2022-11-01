0b48faa21f9a2ff3daebd761f43d9c27e0f098cf
kegcheck <65734212+kegcheck@users.noreply.github.com>
2022-10-31 19:55:06 -0500

    update email

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -14,7 +14,7 @@ const ContactPage = () => {
                         <p className='my-0 fs-3 fw-light' style={{letterSpacing: '2px', fontFamily: 'sans-serif'}}>dwyerjakej@gmail.com</p>
                     </div>
                     <Form className='w-100' action="https://formsubmit.co/31b3c32ff56df3ad8800eb7f3d8a14fc" method="POST">
-                        <input type="hidden" name="_next" value="https://jakedwyer.com/success"></input>
+                        <input type="hidden" name="_next" value="https://www.jakedwyer.com/success"></input>
                         <Form.Group className='mb-3'>
                             <Form.Label>Name</Form.Label>
                             <Form.Control type="text" name="name" required placeholder='Enter your name'></Form.Control>
