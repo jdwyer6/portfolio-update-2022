dc5d97c40f0cf117cfec9b58a6c25c6acac2f91c
kegcheck <65734212+kegcheck@users.noreply.github.com>
2022-11-08 09:40:29 -0600

    add projects section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,11 +12,12 @@ import dog from '../assets/images/dog.png';
 import clockedin from '../assets/images/clockedin.png';
 import { BsStackOverflow } from "react-icons/bs";
 import Button_Primary from "../components/Button_Primary";
+import clockedInBG from "../assets/images/ClockedInBG.jpg";
 
 const HomePage = () => {
     return ( 
         <>
-            <Container fluid>
+            <Container fluid className='position-relative px-0'>
                 <Row className='height-screen d-flex align-items-center position-relative'>
                     <Col className='position-relative text-center'>
                         <h1 style={{zIndex: '5', position: 'relative'}}>JAKE DWYER</h1>
@@ -31,8 +32,149 @@ const HomePage = () => {
                         <Button_Primary text='GO TO PROJECTS'/>
                         <p className="my-3">Software Developer</p>
                     </Col>
-                    
+                </Row>
+                <div className='gradient-vertical'>
+                    <Row>
+                        <Col md='4'>
+                            <h1 className='text-start'>WHO</h1>
+                            <h1 className='text-start'>AM</h1>
+                            <h1 className='text-start'>I?</h1>
+                        </Col>
+                        <Col>
+                            <h4 className='h4-bold text-start'>SUMMARY</h4>
+                            <ul>
+                                <li>Frontend developer</li>
+                                <li>Fluent in English and Spanish</li>
+                                <li>React, HTML, Javascript, CSS</li>
+                                <li>Fullstack certification (MongoDB, Express, React, NodeJS)</li>
+                                <li>Experience with game development in C#.</li>
+                                <li>8 Years professional education experience.</li>
+                            </ul>
+                        </Col>
+                        <Col>
+                            <h4 className='h4-bold text-start'>EDUCATION</h4>
+                            <div className='mb-3'>
+                                <p className='mb-0'>Full Stack Web Development Certification</p>
+                                <p className='mb-0 p-small'>Nucamp Coding Bootcamp</p>
+                                <p className='mb-0 p-small'>Graduated: 2022 with honors</p>
+                            </div>
+                            <div className='mb-3'>
+                                <p className='mb-0'>Bachelor's Degree in Education</p>
+                                <p className='mb-0 p-small'>Belmont University</p>
+                                <p className='mb-0 p-small'>Graduated: 2014</p>
+                            </div>
+                            <div className='mb-3'>
+                                <p className='mb-0'>Associate Degree in Computer Science</p>
+                                <p className='mb-0 p-small'>St. Charles Community College</p>
+                                <p className='mb-0 p-small'>Expected Graduation: 2025</p>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs='12'>
+                            <h4 className='text-center h4-spaced-dark'>EXPERIENCE</h4>
+                            <div className='d-flex mt-5'>
+                                <Col className='text-center'>
+                                    <p className='mb-0'>2014-2022</p>
+                                    <p className='p-large mb-0'><b>Math and Spanish Educator</b></p>
+                                    <p className='mb-0'>Oakvill High School - St. Louis, MO</p>
+                                    <p className='mb-0 p-small'>2020-2022</p>
+                                    <p className='mb-0'>ASF & Colegio Northridge - Mexico City, MX</p>
+                                    <p className='mb-0 p-small'>2017-2020</p>
+                                    <p className='mb-0'>Belleview Middle School - Nashville, TN</p>
+                                    <p className='mb-0 p-small'>2014-2017</p>
+                                </Col>
+                                <Col className='text-center'>
+                                    <p className='mb-0'>2020-present</p>
+                                    <p className='p-large mb-0'><b>Freelance Web Developer</b></p>
+                                    <p className='mb-0'>Recent Projects:</p>
+                                    <p className='mb-0 p-small'>Farah Architecture</p>
+                                    <p className='mb-0 p-small'>Flecks Appliance Center</p>
+                                    <p className='mb-0 p-small'>FADM Impresos</p>
 
+                                </Col>
+
+                            </div>
+                        </Col>
+                    </Row>
+
+
+                    <Row className='top-space'>
+                        <Col className='d-flex flex-column align-items-center'>
+                            <ul>
+                                <h4 className='h4-bold text-start'>FRONTEND</h4>
+                                <li>React</li>
+                                <li>Redux</li>
+                                <li>Javascript</li>
+                                <li>HTML</li>
+                                <li>CSS/SASS</li>
+                                <li>Bootstrap</li>
+                            </ul>
+                        </Col>
+                        <Col className='d-flex flex-column align-items-center'>
+                            <ul>
+                                <h4 className='h4-bold text-start'>BACKEND</h4>
+                                <li>NodeJS</li>
+                                <li>MongoDB</li>
+                                <li>Express</li>
+                            </ul>
+                        </Col>
+                        <Col className='d-flex flex-column align-items-center'>
+                            <ul>
+                                <h4 className='h4-bold text-start'>OTHER</h4>
+                                <li>Git</li>
+                                <li>Adobe Photoshop</li>
+                                <li>Adobe Premiere</li>
+                                <li>Adobe After Effects</li>
+                                <li>Blender</li>
+                                <li>Unity & C#</li>
+                            </ul>
+                        </Col>
+
+                    </Row>
+                    <div className='d-flex justify-content-center'>
+                        <button className='btn-primary w-75 d-flex justify-content-around' style={{marginTop: '6rem'}}>
+                            <div className='d-flex'>
+                                <p>Email:</p>
+                                <p className='mx-3 p-normal'>dwyerjakej@gmail.com</p>
+                            </div>
+                            <div className='d-flex'>
+                                <p>LinkedIn:</p>
+                                <p className='mx-3 p-normal'>linkedin.com/in/dwyer-jake25</p>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+                <Row className='height-screen d-flex align-items-center position-relative'>
+                    <Col className='position-relative text-center'>
+                        <h1 style={{zIndex: '5', position: 'relative'}}>PROJECTS</h1>
+                        <h1 className='bg-text-large' style={{zIndex: '1', opacity: '25%'}}>PROJECTS</h1>
+                        <h1 className='bg-text-large' style={{zIndex: '3', opacity: '10%', top: '30%'}}>PROJECTS</h1>
+                        <h1 className='bg-text-large' style={{zIndex: '2', opacity: '15%', top: '15%'}}>PROJECTS</h1>
+                        <h1 className='bg-text-large' style={{zIndex: '3', opacity: '10%', top: '70%'}}>PROJECTS</h1>
+                        <h1 className='bg-text-large' style={{zIndex: '4', opacity: '5%', top: '75%'}}>PROJECTS</h1>
+                        <div className='color-splash-1'></div>
+                        <div className='color-splash-2'></div>
+                        <h4 style={{marginTop: '-30px'}}>FEATURED</h4>
+                    </Col>
+                </Row>
+                <Row>
+                    {/* <img className='clock-bg' src={clockedInBG} width='100'></img> */}
+                    <div className='clock-bg position-relative'>
+                        <img src={clockedInBG} style={{visibility: 'hidden'}}/>
+                        <div className='featured-project-text-container'>
+                            <h1 className='text-white text-start'>ClockedIn</h1>  
+                            <p className='text-white p-normal'>A fullstack application offering a streamlined solution for businesses to keep track of employee hours.</p>
+                            <div className='menu d-flex align-items-center' style={{height: '50vh'}}>
+                                <div className="menu-items d-flex flex-column ">
+                                    <a href='' className='menu-item text-decoration-none'><h3 className='text-white'>Visit the site</h3></a>
+                                    <a href='' className='menu-item text-decoration-none'><h3 className='text-white'>Details and Design</h3></a>
+                                    <a href='' className='menu-item text-decoration-none'><h3 className='text-white'>Git Hub Repo</h3></a>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
                     
                 </Row>
             </Container>
